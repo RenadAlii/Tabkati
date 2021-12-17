de4ba3ac33cc682fdbb4a11ba675c0bc3db82f27
Renad <Renad.Ali.ruh.k@tuwaiq.edu.sa>
2021-12-17 23:52:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tabkati/ui/login/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tabkati.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tabkati.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
