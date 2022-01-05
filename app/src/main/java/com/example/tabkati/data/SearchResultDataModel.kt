169d4694ee188c4af307b3eb71e2c41c2050e2ef
Renad <Renad.Ali.ruh.k@tuwaiq.edu.sa>
2022-01-05 23:23:35 +0300

    edit Recipe Details

---
+++ app/src/main/java/com/example/tabkati/data/SearchResultDataModel.kt
@@ -0,0 +1,28 @@
+package com.example.tabkati.data
+
+import com.squareup.moshi.Json
+
+data class SearchResultDataModel(
+
+	@Json(name="number")
+	val number: Int? = null,
+
+	@Json(name="totalResults")
+	val totalResults: Int? = null,
+
+	@Json(name="results")
+	val results: List<ResultsItem?>? = null
+)
+
+data class ResultsItem(
+
+	@Json(name="image")
+	val image: String? = null,
+
+	@Json(name="id")
+	val id: Int? = null,
+
+	@Json(name="title")
+	val title: String? = null,
+
+)
