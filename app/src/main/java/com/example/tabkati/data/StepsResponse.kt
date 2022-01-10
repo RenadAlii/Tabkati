784bc105d00bf1df3937899333d92ad4f0d631c8
Renad <Renad.Ali.ruh.k@tuwaiq.edu.sa>
2022-01-11 00:37:16 +0300

    finish the profile

---
+++ app/src/main/java/com/example/tabkati/data/StepsResponse.kt
@@ -0,0 +1,45 @@
+package com.example.tabkati.data
+
+import com.squareup.moshi.Json
+
+//data class StepsResponse(
+//
+//	@Json(name="StepsResponse")
+//	val stepsResponse: List<StepsResponseItem?>? = null
+//)
+
+data class StepsItem(
+
+	@Json(name="number")
+	val number: Int? = null,
+
+	@Json(name="step")
+	val step: String? = null,
+
+	@Json(name="ingredients")
+	val ingredients: List<Any?>? = null
+
+
+)
+
+data class StepsResponseItem(
+	@Json(name="steps")
+	val steps: List<StepsItem?>? = null
+)
+
+
+
+data class IngredientsItem(
+
+	@Json(name="image")
+	val image: String? = null,
+
+	@Json(name="localizedName")
+	val localizedName: String? = null,
+
+	@Json(name="name")
+	val name: String? = null,
+
+	@Json(name="id")
+	val id: Int? = null
+)
