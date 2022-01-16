97f66115526e1f3336fd7c89704740dbe12d2aba
Renad <Renad.Ali.ruh.k@tuwaiq.edu.sa>
2022-01-16 10:10:09 +0300

    fix google sign progress bar

--- app/src/main/java/com/example/tabkati/ui/recipes/RecipesViewModel.kt
+++ app/src/main/java/com/example/tabkati/ui/recipes/RecipesViewModel.kt
@@ -57,8 +57,8 @@ class RecipesViewModel @Inject constructor(private val repository: RecipesReposi
         }
 
         _status.value = RecipesApiStatus.LOADING
-        getCatories()
         getRandomRecipes()
+        getCatories()
         getRandomRecipesE()
 
         _status.value = RecipesApiStatus.DONE
